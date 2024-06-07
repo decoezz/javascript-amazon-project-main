7626510c11c8178e9f91c9e1959cf18c5dc98b5a
decoezz <ezz111200@gmail.com>
2024-06-07 23:35:50 +0300

    Fix price Formatting and radio selector

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,5 +1,6 @@
 import{cart} from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatcurrency } from './utils/money.js';
 let cartsummaryHTML ='';
 cart.forEach((cartItem)=>{
     const productId = cartItem.productId;
@@ -25,7 +26,7 @@ cart.forEach((cartItem)=>{
                   ${matchingproduct.name}
                 </div>
                 <div class="product-price">
-                  ${matchingproduct.price /100}
+                  ${formatcurrency(matchingproduct.price)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -47,7 +48,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingproduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -60,7 +61,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingproduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -73,7 +74,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingproduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
